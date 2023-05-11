c8bcd95e38601e542a57279dbdce765a512b021d
Matthew Miner <misternuks@gmail.com>
2023-05-11 18:36:47 +0900

    route actions, build controllers and views, begin styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Warming the syrup...'
+
+Movie.destroy_all
+
+puts 'Contacting Dr. Gemima...'
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Marketing.buzzwords,
+    poster_url: "https://randommer.io/images/movies/#{rand(11..20)}.jpg",
+    rating: rand(0..10).to_f
+  )
+end
+puts 'Oh my god, my waffle!'
